15958452ce2502781243c1218dc530cd9d76ba76
Hazo Baikulov <spacehaz@gmail.com>
2026-03-04 16:19:28 +0300

    updates for sdk and redirect url

--- lib/bringid.ts
+++ lib/bringid.ts
@@ -3,4 +3,5 @@ import { BringID } from "bringid";
 export const bringid = new BringID({
   appId: "0xa89cce22e034c0d7b00e91ad59dee7ffc41367a957fe39eb0d98ea7233387584",
   mode: "dev",
+  redirectUrl: "https://farcaster.xyz/miniapps/1NIui4yTg5jf/bringid",
 });
